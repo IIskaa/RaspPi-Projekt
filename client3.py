43aafcfdd5a5b8a416973d34bfbd4dba592dfe48
KhademH <husseini307@gmail.com>
2021-10-01 12:07:20 +0200

    added to payload

--- client3.py
+++ client3.py
@@ -3,30 +3,24 @@ import time
 import struct
 import os
 import glob
-import time
 import datetime
 
-# The callback for when the client receives a CONNACK response from the server.
+
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
 
-# Create a MQTT client and register a callback
-# for connect events
 client = mqtt.Client()
 client.on_connect = on_connect
 
-# Connect to a broker
 client.connect("192.168.137.1", port=1883, keepalive=60)
 
-# Start a background loop that handles all
-# communication with the MQTT broker
 client.loop_start()
 
 
 os.system('modprobe w1-gpio')
 os.system('modprobe w1-therm')
 
-#Hittar temperaturgivarens  sokvag
+
 base_dir = '/sys/bus/w1/devices/'
 device_folder = glob.glob(base_dir + '28*')[0]
 device_file = device_folder + '/w1_slave'
@@ -35,7 +29,6 @@ def read_temp_raw():
     with open(device_file, "r") as temp_read_file:
         return temp_read_file.readlines()
 
-#Omvandlar temperaturdatan till nagot lasbart
 def temp_c():
     lines = read_temp_raw()
     while lines[0].strip()[-3:] != 'YES':
@@ -47,19 +40,19 @@ def temp_c():
         temp_c = int(temp_string)
         return temp_c
 
-timenow = datetime.datetime.now()
 
-# send a random value every second
+
+
 while True:
+
     time.sleep(1)
     id = 3
+    index = 0
     temp = temp_c()
+    timestamp = int(datetime.datetime.now().timestamp())   
     print(temp)
     print(str(temp))
 
-    data = struct.pack("!QIB", id, timenow, temp)
+    data = struct.pack("!QIBiB", id, timestamp, index, temp)
 
-    # publish the data to the topic some/topic
-    # using the packed struct as payload and
-    # MQTT QoS set to 1
     client.publish("yrgo/hispi/iksa/hh", payload=data, qos=1)
--- server.py
+++ server.py
@@ -1,6 +1,8 @@
 import struct
 import paho.mqtt.client as mqtt
 import struct
+from datetime import datetime
+
 # The callback for when the client connects to the server
 def on_connect(client, userdata, flags, rc):
 # Subscribing in on_connect() means that
@@ -8,8 +10,9 @@ def on_connect(client, userdata, flags, rc):
     client.subscribe("yrgo/hispi/iksa/hh", qos=1)
 # The callback for receiving message
 def on_message(client, userdata, msg):
-    id, temp = struct.unpack("!QIB", msg.payload)
-    print(f"message: {id}, {temp}")
+    id, timestamp, index, temp = struct.unpack("!QIBi", msg.payload)
+    time = datetime.fromtimestamp(timestamp)
+    print(f"message: {id}, {temp}, {index}, {time})")
 # Create a MQTT client with callbacks
 client = mqtt.Client()
 client.on_connect = on_connect
