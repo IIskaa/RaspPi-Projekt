433fdb0afd8289dee1261753810976f51dc6010a
KhademH <husseini307@gmail.com>
2021-10-01 10:23:43 +0200

    Ta emot payload

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 import paho.mqtt.client as mqtt
+import struct
 # The callback for when the client connects to the server
 def on_connect(client, userdata, flags, rc):
 # Subscribing in on_connect() means that
@@ -6,7 +7,8 @@ def on_connect(client, userdata, flags, rc):
     client.subscribe("yrgo/hispi/iksa/hh", qos=1)
 # The callback for receiving message
 def on_message(client, userdata, msg):
-    print(f"message: {msg.payload}")
+    id, temp = struct.unpack("!bq", msg.payload)
+    print(f"message: {id}, {temp}")
 # Create a MQTT client with callbacks
 client = mqtt.Client()
 client.on_connect = on_connect
@@ -14,4 +16,5 @@ client.on_message = on_message
 client.connect("192.168.137.1", port=1883, keepalive=60)
 # Blocking call that processes network traffic, dispatches
 # callbacks and handles reconnecting.
+
 client.loop_forever()
